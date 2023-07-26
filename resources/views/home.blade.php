bb7c72afc5ea38bb9a90d74ac0a57123f50d2cd2
AntonelloStefania <antonelloste94@gmail.com>
2023-07-26 20:06:04 +0200

    aggiunto controllo partenza in base alla data odierna

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,84 +15,94 @@
 
 </head>
 
-<body class="d-flex align-items-center ">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 d-flex justify-content-center">
+    <body class="d-flex align-items-center ">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center">
 
-                <table class="table p-3 text-center">
-                    <thead>
-                      <tr>
-                        <th scope="col"><i class="fa-solid fa-train" style="color: #001c4d;"></i></th>
-                        <th scope="col">Azienda</th>
-                        <th scope="col">St.Partenza</th>
-                        <th scope="col">St.Arrivo</th>
-                        <th scope="col">Partenza</th>
-                        <th scope="col">Arrivo</th>
-                        <th scope="col">Treno</th>
-                        <th scope="col">Carrozze</th>
-                        <th scope="col">Ritardo</th>
-                        <th scope="col">Stato</th>
-                      </tr>
-                    </thead>
-                 @foreach($trains as $train)
-                    <tbody>
-                      <tr>
-                        <th scope="row">{{$train->id}}</th>
-                        <td>
-                            <span>
-                                {{$train->azienda}}
-                            </span>
-                        </td>
-                        <td>
-                            <span>
-                                {{$train->stazione_di_partenza}}
-                            </span>
-                        </td>
-                        <td>
-                            <span>
-                                {{$train->stazione_di_arrivo}}
-                            </span>
-                        </td>
-                        <td>
-                            <span>
-                                {{substr($train->orario_partenza, 0,5)}}
-                            </span>
-                        </td>
-                        <td>
-                            <span>
-                                {{substr($train->orario_arrivo, 0,5)}}
-                            </span>
-                        </td>
-                        <td>
-                            <span>
-                                {{$train->codice_treno}}
-                            </span>
-                        </td>
-                        <td>
-                            <span>
-                                {{$train->numero_carrozze}}
-                            </span>
-                        </td>
-                        <td>
-                            <span>
-                                {{$train->in_orario ? '':'+5 min'}}
-                            </span>
-                        </td>
-                        <td>
-                            <span>
-                                <i class="{{$train->cancellato ? 'fa-solid fa-circle-minus':'fa-solid fa-circle-check'}}" style="{{$train->cancellato ? ' color:#f00000': ' color:#03a300'}}"></i>
-                            </span>
-                        </td>
-                      </tr>
-                    </tbody>
-                 @endforeach
-                </table>
+                    <table class="table p-3 text-center">
+                        <thead>
+                        <tr>
+                            <th scope="col"><i class="fa-solid fa-train" style="color: #001c4d;"></i></th>
+                            <th scope="col">Azienda</th>
+                            <th scope="col">St.Partenza</th>
+                            <th scope="col">St.Arrivo</th>
+                            <th scope="col">Partenza</th>
+                            <th scope="col">Arrivo</th>
+                            <th scope="col">Treno</th>
+                            <th scope="col">Carrozze</th>
+                            <th scope="col">Ritardo</th>
+                            <th scope="col">Stato</th>
+                        </tr>
+                        </thead>
+                    @foreach($trains as $train)
+                        @if($train->data_partenza === $train->data_odierna)
+                        <tbody>
+                        <tr>
+                            <th scope="row">{{$train->id}}</th>
+                            <td>
+                                <span>
+                                    {{$train->azienda}}
+                                </span>
+                            </td>
+                            <td>
+                                <span>
+                                    {{$train->stazione_di_partenza}}
+                                </span>
+                            </td>
+                            <td>
+                                <span>
+                                    {{$train->stazione_di_arrivo}}
+                                </span>
+                            </td>
+                            <td>
+                                <span>
+                                    {{substr($train->orario_partenza, 0,5)}}
+                                </span>
+                            </td>
+                            <td>
+                                <span>
+                                    {{substr($train->orario_arrivo, 0,5)}}
+                                </span>
+                            </td>
+                            <td>
+                                <span>
+                                    {{$train->codice_treno}}
+                                </span>
+                            </td>
+                            <td>
+                                <span>
+                                    {{$train->numero_carrozze}}
+                                </span>
+                            </td>
+                            <td>
+                                <span>
+                                    {{$train->in_orario ? '':'+5 min'}}
+                                </span>
+                            </td>
+                            <td>
+                                <span>
+                                    <i class="{{$train->cancellato ? 'fa-solid fa-circle-minus':'fa-solid fa-circle-check'}}" style="{{$train->cancellato ? ' color:#f00000': ' color:#03a300'}}"></i>
+                                </span>
+                            </td>
+                        </tr>
+                        </tbody>
+                        @endif
+                        
+                        @endforeach
+                    </table>
+                    
+                </div>
+                @if($train->data_partenza !== $train->data_odierna)
+               
+                <div class="bg-danger text-white text-center col-6 offset-3">
+                    <h1>nessun treno in partenza oggi</h1>
+                </div>
+                @endif
             </div>
         </div>
-    </div>
 
 
-</body>
+    </body>
 
 </html>
