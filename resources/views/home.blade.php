c314f6bb5df7ba71e7e5202b80625f69624bad89
AntonelloStefania <antonelloste94@gmail.com>
2023-07-26 17:12:41 +0200

    mostrati dati del db a schermo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,11 +15,83 @@
 
 </head>
 
-<body>
+<body class="bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col">
 
-   @foreach($trains as $train)
-   ciao
-   @endforeach
+                <table class="table p-3">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Azienda</th>
+                        <th scope="col">St.Partenza</th>
+                        <th scope="col">St.Arrivo</th>
+                        <th scope="col">Partenza</th>
+                        <th scope="col">Arrivo</th>
+                        <th scope="col">Treno</th>
+                        <th scope="col">Carrozze</th>
+                        <th scope="col">Ritardo</th>
+                        <th scope="col">Stato</th>
+                      </tr>
+                    </thead>
+                 @foreach($trains as $train)
+                    <tbody>
+                      <tr>
+                        <th scope="row">{{$train->id}}</th>
+                        <td>
+                            <span>
+                                {{$train->azienda}}
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                                {{$train->stazione_di_partenza}}
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                                {{$train->stazione_di_arrivo}}
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                                {{$train->orario_partenza}}
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                                {{$train->orario_arrivo}}
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                                {{$train->codice_treno}}
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                                {{$train->numero_carrozze}}
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                                
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                                
+                            </span>
+                        </td>
+                      </tr>
+                    </tbody>
+                 @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
+
 
 </body>
 
