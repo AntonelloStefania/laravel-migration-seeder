b1a93761b7d2a956e21e17fe8b4f03e618002a30
AntonelloStefania <antonelloste94@gmail.com>
2023-07-27 14:52:57 +0200

    iniziata compilazione seeders

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use app\Models\Train;
 
+use Faker\Provider\en_US\Address;
+
 class TrainTableSeeder extends Seeder
 {
     /**
@@ -15,8 +17,17 @@ class TrainTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
+        for($i=0; $i<6; $i++){
+            $train = new Train();
+            $train->azienda = $faker->word();
+            $train->stazione_di_partenza->city();
+            $train->stazione_di_arrivo->city();
+            $train->orario_partenza-> 
+            $train->orario_arrivo->
+            $train->codice_treno->
+        }
         
     }
 }
