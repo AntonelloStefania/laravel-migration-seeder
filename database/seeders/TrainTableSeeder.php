3372f947b0d0aed306242ec796713a8c6b7327b6
AntonelloStefania <antonelloste94@gmail.com>
2023-07-27 14:14:43 +0200

    importazione pacchetto Faker e creazione file seeder

---
+++ database/seeders/TrainTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+use app\Models\Train;
+
+class TrainTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+    }
+}
